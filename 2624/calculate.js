1463c23a7efe5b2037a9e5b7c0be850c42e0aa36
Minh Bui <sunrsie8vn@gmail.com>
2019-09-06 10:49:11 +0700

    -2624

---
+++ 2624/calculate.js
@@ -0,0 +1,24 @@
+function cals(buttonValue) 
+{
+    if (buttonValue == 'AC') 
+    {
+       document.getElementById('valueshow').value = '0';
+    }
+    else
+    {
+        if (document.getElementById('valueshow').value == '0') 
+        {
+            document.getElementById('valueshow').value = buttonValue;
+        }
+        else
+        {
+            document.getElementById('valueshow').value += buttonValue;
+        }
+    } 
+}
+
+function cal(equation)  
+{
+    var answer = eval(equation);
+    document.getElementById('valueshow').value = answer;
+}
