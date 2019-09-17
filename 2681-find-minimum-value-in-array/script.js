c430e2947738b6a560fe87140392d03378ed1f34
Minh Bui <sunrsie8vn@gmail.com>
2019-09-17 10:32:10 +0700

    2681

---
+++ 2681-find-minimum-value-in-array/script.js
@@ -0,0 +1,27 @@
+
+let arr1 = [3, 5, 1, 8, -3, 7, 8];
+let arr2 = [7, 12, 6, 9, 20, 56, 89];
+let arr3 = [];
+let arr4 = [0, 0, 0, 0, 0, 0];
+let position = positionMinArray(arr1);
+if (position == -1) {
+	alert("Empty array");
+}
+else {
+	alert("Minimum value in position " + position);	
+}
+
+function positionMinArray(arr) {
+    if(arr.length == 0)
+        return -1;
+    let min = arr[0];
+    let position = 0;
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+            position = i;
+        }
+    }
+    return position;
+}
