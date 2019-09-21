ea7c45e8db2b83b761ca2b81184ceb735902d6f3
Minh Bui <sunrsie8vn@gmail.com>
2019-09-21 21:26:00 +0700

    2695

--- 2695-exercise-build-a-class-that-describes-temperature/script.js
+++ 2695-exercise-build-a-class-that-describes-temperature/script.js
@@ -1,27 +1,28 @@
 class Temperature {
 	constructor(tempC) {
-		this.tempC = function() {
-			this.tempC = tempC;
-		}
+		this.tempC = tempC;
+	}
+
+	getTempC() {
+		if(this.tempC < -273)
+			return -273
+		return this.tempC;
 	}
 
 	convertC2F() {
-		if(this.tempC < -273)
-			this.tempC = -273;
-		return this.tempC * 9 / 5 + 32;
+		return this.getTempC() * 9 / 5 + 32;
 	}
 
 	convertC2K() {
-		if(this.tempC < -273)
-			this.tempC = -273;
-		return this.tempC + 273.15;
+		return this.getTempC() + 273.15;
 	}
 
 	getInfoTemp() {
-		return "Temperature:<br>°C = " + this.tempC + "°" + "<br>°K = " + this.convertC2K() + "°" + "<br>°F = " + this.convertC2F() + "°";
+		return "Temperature:<br>°C = " + this.getTempC() + "°" + "<br>°K = " + this.convertC2K() + "°" + "<br>°F = " + this.convertC2F() + "°";
 	}
 }
 
 var tempObj = new Temperature();
 tempObj.tempC = 25;
+tempObj.getTempC();
 document.write(tempObj.getInfoTemp());
